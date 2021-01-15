68283a797b3713b727e6d51be34198a70858ab17
tsubauaaa <tsubasa1173@gmail.com>
2021-01-16 00:45:18 +0900

    ランレングス圧縮を習う

--- AGC/39/a.py
+++ AGC/39/a.py
@@ -1,13 +1,8 @@
-S = list(input())
-K = int(input())
-T = S * 2
-cnt = 0
-for i in range(len(T) - 1):
-    if T[i] == T[i + 1]:
-        cnt += 1
-        T[i + 1] = "0"
-print(T)
-if S[0] == S[-1]:
-    if T[-1] != "0":
-        d = 1
-print(int(cnt / 2 * K))
+from itertools import groupby
+
+S = input()  # issii
+for c, g in groupby(S):
+    print(c, len(list(g)))
+    # i 1
+    # s 2
+    # i 2
