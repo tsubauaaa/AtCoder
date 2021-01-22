ee99a2ad600c9d07542ada4524dad4f25a539b41
tsubauaaa <tsubasa1173@gmail.com>
2021-01-22 16:15:30 +0900

    完全なる解説AC

--- AGC/39/a.py
+++ AGC/39/a.py
@@ -1,8 +1,21 @@
 from itertools import groupby
 
-S = input()  # issii
+S = input()
+K = int(input())
+runlength = []
 for c, g in groupby(S):
-    print(c, len(list(g)))
-    # i 1
-    # s 2
-    # i 2
+    runlength.append(len(list(g)))
+cnt = 0
+for rl in runlength:
+    if rl >= 2:
+        cnt += rl // 2
+
+if len(runlength) == 1:
+    print(runlength[0] * K // 2)
+    exit()
+
+if S[0] == S[-1] and runlength[0] % 2 != 0 and runlength[-1] % 2 != 0:
+    cnt += 1
+    print(cnt * K - 1)
+else:
+    print(cnt * K)
