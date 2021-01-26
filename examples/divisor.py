39401880457ebb97644f8dc97f66a0ec1d288c81
tsubauaaa <tsubasa1173@gmail.com>
2021-01-26 21:52:00 +0900

    不要なファイルを削除

--- examples/divisor.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-div = []
-i = 1
-while i * i <= N:
-    if N % i == 0:
-        div.append(i)
-        div.append(N // i)
-    i += 1
-print(div)
--- examples/dp_fibonacci.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-dp = [1] * (N + 1)
-dp[1] = 1
-
-for i in range(2, N + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
-
-print(dp)
