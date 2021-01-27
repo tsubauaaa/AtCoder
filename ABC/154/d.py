cbb1cbac2c7466320b176e44d1a28766f2a2ed50
tsubauaaa <tsubasa1173@gmail.com>
2021-01-27 22:08:56 +0900

    温存していたやつ

--- ABC/154/d.py
+++ ABC/154/d.py
@@ -1,2 +1,13 @@
 N, K = map(int, input().split())
 p = list(map(int, input().split()))
+cs_p = [(p[0] + 1) / 2]
+for pi in p[1:]:
+    cs_p.append(cs_p[-1] + (pi + 1) / 2)
+if N == K:
+    print(cs_p[-1])
+    exit()
+
+ans = 0
+for i in range(N - K):
+    ans = max(ans, cs_p[i + K] - cs_p[i])
+print(ans)
