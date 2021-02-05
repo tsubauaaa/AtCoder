0977f3be9bc0f8c4fda65a19e5d20a731d2cc847
tsubauaaa <tsubasa1173@gmail.com>
2021-02-05 20:28:47 +0900

    虚無ります

---
+++ ABC/16/b.py
@@ -0,0 +1,11 @@
+A, B, C = map(int, input().split())
+ans = ""
+if A + B == C and A - B == C:
+    ans = "?"
+elif A + B == C:
+    ans = "+"
+elif A - B == C:
+    ans = "-"
+else:
+    ans = "!"
+print(ans)
