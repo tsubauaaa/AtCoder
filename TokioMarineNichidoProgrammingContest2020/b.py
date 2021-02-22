d0bdd1feb9b305d940c282dbb7f53ab12160cb72
tsubauaaa <tsubasa1173@gmail.com>
2021-02-22 21:31:33 +0900

    積年のEasyを晴らした

--- TokioMarineNichidoProgrammingContest2020/b.py
+++ TokioMarineNichidoProgrammingContest2020/b.py
@@ -1,8 +1,14 @@
 A, V = map(int, input().split())
 B, W = map(int, input().split())
 T = int(input())
-
-if V < W:
-    print('NO')
+if V == W:
+    print("NO")
     exit()
-
+if A < B:
+    x = (B - A) / (V - W)
+else:
+    x = (B - A) / (W - V)
+if 1 <= x <= T:
+    print("YES")
+else:
+    print("NO")
