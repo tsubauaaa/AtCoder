928b8c36e43e81e24db4ce3e74a7aa0bc0230c3d
tsubauaaa <tsubasa1173@gmail.com>
2021-03-02 17:16:02 +0900

    虚無すぎっす

--- ABC/46/a.py
+++ ABC/46/a.py
@@ -1,7 +1,2 @@
-N = int(input())
-z = []
-for i in range(1, 600000):
-    j = set(list(str(i)))
-    if len(j) == 1:
-        z.append(i)
-print(z[N - 1])
+abc = set(map(int, input().split()))
+print(len(abc))
