c15f935041e63b1f2833b8bc2d4491d68e762448
tsubauaaa <tsubasa1173@gmail.com>
2021-03-08 14:02:10 +0900

    なんかやりかけている

--- ABC/56/b.py
+++ ABC/56/b.py
@@ -1,2 +1,9 @@
 W, a, b = map(int, input().split())
 print(a, a + W, b, b + W)
+
+if b < a + W and a + W < a + W:
+    print(a + W - b)
+elif a < b and b + W < a:
+    print(b - a)
+else:
+    print(0)
