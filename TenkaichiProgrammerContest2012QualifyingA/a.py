d2c867e0e710a6a7fba504e6627f8bf880b642b0
tsubauaaa <tsubasa1173@gmail.com>
2021-04-13 21:10:06 +0900

    フィボナッチであることに気づけたかどうか、、

--- TenkaichiProgrammerContest2012QualifyingA/a.py
+++ TenkaichiProgrammerContest2012QualifyingA/a.py
@@ -1,2 +1,10 @@
 n = int(input())
-print(n + (n - 3) * (n - 2) / 2)
+if n == 0:
+    print(1)
+    exit()
+dp = [1] * (n + 1)
+dp[1] = 1
+
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+print(dp[-1])
