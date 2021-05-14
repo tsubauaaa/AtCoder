ff17d72e4411a2c4d1aaf39cfd4541f55785bb2c
tsubauaaa <tsubasa1173@gmail.com>
2021-05-14 22:22:03 +0900

    前半後半分割して、A,Bがどちらに位置するかで前半後半ごと処理する

--- ABC/199/c.py
+++ ABC/199/c.py
@@ -1,15 +1,20 @@
-from collections import deque
 from sys import stdin
 
 N = int(input())
-S = input()
+S = list(input())
 Q = int(input())
+first = S[:N]
+second = S[N:]
+
 for i in range(Q):
     T, A, B = map(int, stdin.readline().split())
     if T == 1:
-        S = "".join([S[: A - 1], S[B - 1], S[A : B - 1], S[A - 1], S[B:]])
+        if B <= N:
+            first[A - 1], first[B - 1] = first[B - 1], first[A - 1]
+        elif A <= N and B > N:
+            first[A - 1], second[B - 1 - N] = second[B - 1 - N], first[A - 1]
+        else:
+            second[A - 1 - N], second[B - 1 - N] = second[B - 1 - N], second[A - 1 - N]
     else:
-        items = deque(list(S))
-        items.rotate(N * -1)
-        S = "".join(list(items))
-print(S)
+        first, second = second, first
+print("".join(first + second))
