0ce9c7b3abd748e7f9657898d5e3769559968ebd
ts-hirota <ts-hirota@kddi.com>
2021-05-25 22:16:08 +0900

    なんかベタ書きだったけどよかったかな

---
+++ ABC/21/a.py
@@ -0,0 +1,30 @@
+N=int(input())
+
+if N in [1,2,4,8]:
+    print(1)
+    print(N)
+elif N == 3:
+    print(2)
+    print(1)
+    print(2)
+elif N == 5:
+    print(2)
+    print(1)
+    print(4)
+elif N == 6:
+    print(2)
+    print(2)
+    print(4)
+elif N == 7:
+    print(3)
+    print(1)
+    print(2)
+    print(4)
+elif N == 9:
+    print(2)
+    print(1)
+    print(8)
+else:
+    print(2)
+    print(2)
+    print(8)
