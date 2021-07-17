ea46d2edd6e870e6462258a3c6f0265f56053a53
tsubauaaa <tsubasa1173@gmail.com>
2021-07-18 00:16:52 +0900

    以外にあがりました。

--- ABC/210/c.py
+++ ABC/210/c.py
@@ -1,14 +1,19 @@
-N,K=map(int,input().split())
-c = list(map(int,input().split()))
-def cumsum(xs):
-  result = [xs[0]]
-  for x in xs[1:]:
-    result.append(result[-1] + x)
-  return result
+import copy
+from collections import Counter
 
-csum = cumsum(c)
-print(csum)
+N, K = map(int, input().split())
+c = list(map(int, input().split()))
+
+counts_dic = {ci: 0 for ci in c}
+counts_list = [{} for _ in range(N)]
+for i in range(N):
+    counts_dic[c[i]] += 1
+    counts_list[i] = copy.deepcopy(counts_dic)
+
 ans = 0
-for i in range(N-K+1):
-    print(csum[i+K-1] - csum[i])
-
+for i in range(N):
+    if i % (K - 1) == 0:
+        ans = max(
+            ans, len(Counter(counts_list[i]) - Counter(counts_list[i - K])))
+        # print(Counter(counts_list[i]) - Counter(counts_list[i - K]))
+print(ans)
