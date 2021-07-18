91288accbf12b86e2fadb822d0d27247f4a5a600
ts-hirota <ts-hirota@kddi.com>
2021-07-18 22:09:13 +0900

    初期とかぜんていとかがよわいよねぇ

--- ABC/210/c.py
+++ ABC/210/c.py
@@ -1,19 +1,15 @@
-import copy
 from collections import Counter
-
 N, K = map(int, input().split())
 c = list(map(int, input().split()))
+counter = Counter(c[:K])
+ans = len(counter)
+for i in range(K, N):
+    left = c[i - K]
+    right = c[i]
+    counter[left] -= 1
+    counter[right] += 1
+    if counter[left] == 0:
+        del counter[left]
+    ans = max(ans, len(counter))
 
-counts_dic = {ci: 0 for ci in c}
-counts_list = [{} for _ in range(N)]
-for i in range(N):
-    counts_dic[c[i]] += 1
-    counts_list[i] = copy.deepcopy(counts_dic)
-
-ans = 0
-for i in range(N):
-    if i % (K - 1) == 0:
-        ans = max(
-            ans, len(Counter(counts_list[i]) - Counter(counts_list[i - K])))
-        # print(Counter(counts_list[i]) - Counter(counts_list[i - K]))
 print(ans)
