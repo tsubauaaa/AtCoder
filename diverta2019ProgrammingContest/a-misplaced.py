102ceb48effa6d8d5fd9e07d1b8572e4eb8b4f63
tsubauaaa <tsubasa1173@gmail.com>
2022-02-09 10:05:08 +0900

    ちゃんと考えたらvoidだった

---
+++ diverta2019ProgrammingContest/a-misplaced.py
@@ -0,0 +1,5 @@
+N, K = map(int, input().split())
+
+ball = N - K
+
+print(ball if K != 1 else 0)
--- diverta2019ProgrammingContest/a.py
+++ diverta2019ProgrammingContest/a.py
@@ -1,5 +1,7 @@
 N, K = map(int, input().split())
+# nums = [i for i in range(1, N + 1)]
+# ans = 0
 
-ball = N - K
-
-print(ball if K != 1 else 0)
+# for i in range(N - K + 1):
+#     print(nums[i])
+print(N - K + 1)
