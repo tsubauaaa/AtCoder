e05034f7a629bc6c7b28d53c92abb2fdbc7ae82a
tsubauaaa <tsubasa1173@gmail.com>
2022-12-10 23:20:14 +0900

    ばり下がった

---
+++ ABC/281/b.py
@@ -0,0 +1,113 @@
+S = input()
+
+if len(S) < 8:
+    print("No")
+    exit()
+
+first = S[0]
+last = S[-1]
+
+if first not in [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]:
+    print("No")
+    exit()
+
+if last not in [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]:
+    print("No")
+    exit()
+
+others = S[1:-1]
+
+for o in others:
+    if o in [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]:
+        print("No")
+        exit()
+
+others = int(others)
+
+if len(str(others)) != 6:
+    print("No")
+    exit()
+
+
+print("Yes")
